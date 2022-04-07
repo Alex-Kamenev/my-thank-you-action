b663a2638926f0702abbd3bb55eff5bd225f4911
aleks <akamenev64@gmail.com>
2022-04-07 10:29:33 -0400

    saying thank you to new pull requests

--- src/action.js
+++ src/action.js
@@ -1,5 +1,19 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
 async function run() {
-  console.log('Hello, world!');
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Thank you fot submitting a pull request! We will try to review it as soon as possible',
+  });
 }
 
 run();
